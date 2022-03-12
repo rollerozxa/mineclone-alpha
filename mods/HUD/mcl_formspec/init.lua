f7cb2f8829c693b33dd9603547c83fbb23581f9b
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-03-12 21:09:41 +0100

    Merge mcl_formspec_prepend into mcl_formspec

--- mods/HUD/mcl_formspec/init.lua
+++ mods/HUD/mcl_formspec/init.lua
@@ -9,3 +9,7 @@ function mcl_formspec.get_itemslot_bg(x, y, w, h)
 	end
 	return out
 end
+
+minetest.register_on_joinplayer(function(player)
+	player:set_formspec_prepend(mcl_vars.gui_nonbg .. mcl_vars.gui_bg_color .. mcl_vars.gui_bg_img)
+end)
